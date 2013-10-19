05cf8290d80aac4406c3d4d8f77963c80eb27da2
Austin Kwok <austinkwok615@gmail.com>
2013-10-19 16:14:05 -0700

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Csc667::Application.routes.draw do
+  root to: "pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
