54187d5e7d952e2995cb3514657d7812e9984b03
Austin Kwok <austinkwok615@gmail.com>
2013-11-24 16:38:18 -0800

    Admin control added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131107034542) do
+ActiveRecord::Schema.define(version: 20131125002900) do
 
   create_table "test_tables", force: true do |t|
     t.datetime "created_at"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20131107034542) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
