bb63579fa2770ff88a6505aa9de7efa9f3af25d4
Bianca Uy <uy.biancamarie@gmail.com>
2013-11-30 01:27:31 -0800

    trying again.

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,15 @@
+class FriendshipsController < ApplicationController
+  def create
+    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+    if @friendship.save
+      flash[:notice] = "Added friend."
+      redirect_to users_path(current_user.id)
+    else
+      flash[:notice] = "Unable to add friend."
+      redirect_to users_path(current_user.id)
+    end
+  end
+
+  def index
+  end
+end
