7c7afc27f9d31d8194daacb5870bc1d5783ff3e4
Bianca Uy <uy.biancamarie@gmail.com>
2013-12-01 23:10:07 -0800

    Add user to Goal table after they sign up.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      Goal.create(:user_id => @user.id)
       redirect_to @user
     else
       render 'new'
