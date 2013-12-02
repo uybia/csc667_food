92827320917d33f14ab3df783d0a5694ba85bc75
Bianca Uy <uy.biancamarie@gmail.com>
2013-12-01 23:21:49 -0800

    Fixing issues.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      Goal.create(:user_id => @user.id)
+      Goal.create(:user_id => @user.id, :calories => 2000)
       redirect_to @user
     else
       render 'new'
