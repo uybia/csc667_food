d88ab94a5901c0fe181b483393a48115efeaa25a
Bianca Uy <uy.biancamarie@gmail.com>
2013-12-03 14:27:59 -0800

    Changed route.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       Goal.create(:user_id => @user.id, :calories => 2000)
-      redirect_to  users_path(:id => @user.id)
+      redirect_to root_path 
     else
       render 'new'
     end
