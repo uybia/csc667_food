d55373e53fe230717b7b430058376b1262668a3a
Bianca Uy <uy.biancamarie@gmail.com>
2013-12-03 15:05:59 -0800

    .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       Goal.create(:user_id => user.id, :calories => 2000)
-      redirect_to root_path 
+      redirect_to signup
     else
       render 'new'
     end
