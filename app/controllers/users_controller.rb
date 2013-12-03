ee1136907d1bac2f0867417a9542cd62b4bf58dc
Bianca Uy <uy.biancamarie@gmail.com>
2013-12-03 08:46:00 -0800

    Modified user files again.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-	if params[:date].present? 
-       date = params[:date]
-    else
-       date = Time.now.strftime("%F")
-    end
-	@user = User.find(@user.id)
-    @meals = @user.meals.where('date = ? ', date ).group("id")
+    date = Time.now.strftime("%F")
+    user = User.find(current_user.id)
+    meals = @user.meals.where('date = ? ', date ).group("id")
     @breakfasts = @meals.where(meal_tag: "breakfast").group("meal_tag")
     @lunches = @meals.where(meal_tag: "lunch")
     @dinners = @meals.where(meal_tag: "dinner") 
-    @date =  Date.parse(date)
-    @calories = @user.goal.calories
   end
 
   def index
