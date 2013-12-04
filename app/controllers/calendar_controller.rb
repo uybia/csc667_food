3aff0928437ae3db34fa5c11379624de11bc72f4
Bianca Uy <uy.biancamarie@gmail.com>
2013-12-04 01:47:58 -0800

    Adding calendar.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,4 +1,11 @@
 class CalendarController < ApplicationController
+<<<<<<< HEAD
+  
+  def show
+    @user = current_user
+    @meals = @user.meals.where("meal_tag = ?", params[:meal_tag])
+    render :json => @meals.to_json(:include  => :food)
+=======
 
   def show 
     @meals = Meal.where("strftime('%Y', date) = ? AND strftime('%m', date) = ?", params[:year], params[:month])
@@ -6,6 +13,7 @@ class CalendarController < ApplicationController
   end
 
   def index
+>>>>>>> d185f359c32dfe61d144ea4e8c062f6e00a6914c
   end
 
 end
