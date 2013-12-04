aa34217e9f5cdf0556b7f537e9041d16d3be9fda
Bianca Uy <uy.biancamarie@gmail.com>
2013-12-04 02:09:40 -0800

    Added calendar files.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,19 +1,9 @@
 class CalendarController < ApplicationController
-<<<<<<< HEAD
-  
-  def show
-    @user = current_user
-    @meals = @user.meals.where("meal_tag = ?", params[:meal_tag])
-    render :json => @meals.to_json(:include  => :food)
-=======
 
   def show 
-    @meals = Meal.where("strftime('%Y', date) = ? AND strftime('%m', date) = ?", params[:year], params[:month])
-   render :json => @meals.to_json(:include  => :food)
-  end
-
-  def index
->>>>>>> d185f359c32dfe61d144ea4e8c062f6e00a6914c
+    @user = current_user
+    @meals = @user.meals.where("meal_tag = ?", params[:meal_tag])
+    render :json => @meals.to_json(:include => :food)
   end
 
 end
