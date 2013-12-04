dfefe97c40f6a10176df4c42cb22735a6af84c17
Bianca Uy <uy.biancamarie@gmail.com>
2013-12-04 14:07:52 -0800

    Adding changes to the GroceryList and ListItems.

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -1,14 +1,27 @@
 class GroceryListsController < ApplicationController
+    before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
     def index
+        if params[:date].present? 
+            date = params[:date]
+        else
+            date = Time.now.strftime("%F")
+        end
         @lists = GroceryList.where("user_id = ?", current_user.id)
         @list = GroceryList.new
+        @date = Date.parse(date)
         @items = ListItem.find_all_by_list_id(params[:id])  
     end
     
     def show
+        if params[:date].present? 
+            date = params[:date]
+        else
+            date = Time.now.strftime("%F")
+        end
         @list = GroceryList.find(params[:id])
         @items = ListItem.new
-        @item = ListItem.where('list_id = ?', params[:id])  
+        @item = ListItem.where('list_id = ?', params[:id])
+        
     end
     
     def new
@@ -16,15 +29,16 @@ class GroceryListsController < ApplicationController
     end
     
     def create
-        if params[:date].present?
-            date = params[:date]
-        else
-            date = Time.now.strftime("%F")
-        end
+      #  if params[:date].present?
+      #      date = params[:date]
+      #  else
+      #      date = Time.now.strftime("%F")
+      #  end
         @list = GroceryList.new(param)
         
         @list.user_id = (current_user.id)
-        @list.date = Date.parse(date)
+       # @date = Date.parse(date)
+        #@list.date = Date.parse(date)
         if @list.save
             redirect_to @list
         else
@@ -32,10 +46,15 @@ class GroceryListsController < ApplicationController
         end
     end
     
+    def destroy
+        @list = GroceryList.find(params[:id]).destroy
+        redirect_to :back
+    end
+    
     private
     
     def param
-      params.require(:grocery_list).permit(:list_id)
+      params.require(:grocery_list).permit(:list_id, :date)
     end
 
 end
--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -4,13 +4,28 @@ class ListItemsController < ApplicationController
   end
   
   def create
+     if params[:date].present? 
+            date = params[:date]
+        else
+            date = Time.now.strftime("%F")
+        end
     @items = ListItem.new(param)
     @items.list_id = params[:id]
     if @items.save
       redirect_to(:back)
     end
+    @list = GroceryList.find(params[:id])
+    if(@list.update_attributes(params[:date]))
+          #  @date = Date.parse(date)
+      else
+    end
+        
   end
   
+  def destroy
+        @items = ListItem.find( params[:id]).destroy
+        redirect_to :back
+    end
   private
     
     def param
