766757b95ca7842f95fa7e1fd58e7bbb8a9415bb
Bianca Uy <uy.biancamarie@gmail.com>
2013-12-03 16:02:53 -0800

    Changed to old settings.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       Goal.create(:user_id => user.id, :calories => 2000)
-      redirect_to  root_path
+      redirect_to root_path
     else
       render 'new'
     end
