2ff1c9e809552bee7f7c7ecbd4f854da3eba5734
Bianca Uy <uy.biancamarie@gmail.com>
2013-12-04 17:57:35 -0800

    Fixed food form. Added datepicker gem.;

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
+//= require bootstrap-datepicker
 //= require turbolinks
 //= require bootstrap
 //= require jquery.qtip.js
